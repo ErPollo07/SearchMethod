766614752a037f08545963bca6c2d778f3513f9b
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-04-20 11:46:13 +0200

    Fix some bugs in orderUp and Down, add mergeArray method

--- JavaProject/src/Methods.java
+++ JavaProject/src/Methods.java
@@ -1,6 +1,9 @@
 public class Methods {
     public static void main(String[] args) {
+        int[] testArr = {1, 2, 3, 7, 5, 6, 4};
+        int[] destArray = new int[testArr.length];
 
+        destArray = evenOddSearch(testArr);
     }
 
     private static void wrongIndexSearch() {
@@ -35,22 +38,38 @@ public class Methods {
         }
 
         // Order the arrays with 2 calls of the method oderArray
-        orderArray(evenArray, indexEven);
-        orderArray(oddArray, indexOdd);
+        orderUpArray(evenArray, indexEven);
+        orderDownArray(oddArray, indexOdd);
 
         // put the arrays, evenArray and oddArray, together.
+        finalArray = mergeArrays(evenArray, indexEven, oddArray, indexOdd);
 
         return finalArray;
     }
 
-    private static void orderArray(int[] arr, int index) {
-        int numMin, posOfnMin, temp;
+    private static int[] mergeArrays(int[] arr1, int indexArr1, int[] arr2, int indexArr2) {
+        int[] finalArray = new int[indexArr1 + indexArr2];
+        int indexFinal = 0;
+
+        for (int i = 0; i < indexArr1; i++) {
+            finalArray[indexFinal++] = arr1[i];
+        }
+
+        for (int i = 0; i < indexArr2; i++) {
+            finalArray[indexFinal++] = arr2[i];
+        }
+
+        return finalArray;
+    }
+
+    private static void orderUpArray(int[] arr, int index) {
+        int numMin, posOfnMin = 0, temp;
 
         for (int i = 0; i < index; i++) {
             numMin = arr[i];
             // Get the minimum number
             for (int j = i; j < index; j++){
-                if (arr[i] < numMin) {
+                if (arr[j] < numMin) {
                     numMin = arr[j];
                     posOfnMin = j;
                 }
@@ -60,6 +79,26 @@ public class Methods {
             temp = arr[i];
             arr[i] = numMin;
             arr[posOfnMin] = temp;
+        }
+    }
+
+    private static void orderDownArray(int[] arr, int index) {
+        int numMax, posOfnMax = 0, temp;
+
+        for (int i = 0; i < index; i++) {
+            numMax = arr[i];
+            // Get the minimum number
+            for (int j = i; j < index; j++){
+                if (arr[j] > numMax) {
+                    numMax = arr[j];
+                    posOfnMax = j;
+                }
+            }
+
+            // switch position of the numMin with even[i]
+            temp = arr[i];
+            arr[i] = numMax;
+            arr[posOfnMax] = temp;
         }
     }
 
