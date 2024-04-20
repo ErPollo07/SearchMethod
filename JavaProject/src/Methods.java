464009048391b58fd04ed11cf74ecdc4be6bb609
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-04-20 10:37:29 +0200

    Init Commit

---
+++ JavaProject/src/Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+    public static void main(String[] args) {
+
+    }
+
+    private static void wrongIndexSearch() {
+
+    }
+    private static void evenOddSearch() {
+
+    }
+    private static void biBinarySearch() {
+
+    }
+}
