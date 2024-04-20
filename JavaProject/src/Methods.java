175ee79f496ca39ff46e49eb49572f4566bd27bf
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-04-20 10:51:28 +0200

    Add the titles and a short desription of the project in README.md

--- JavaProject/src/Methods.java
+++ JavaProject/src/Methods.java
@@ -6,9 +6,25 @@ public class Methods {
     private static void wrongIndexSearch() {
 
     }
-    private static void evenOddSearch() {
 
+    /*
+    Creare il metodo evenOddSearch che dato un vettore,
+    ordini i numeri pari in modo crescente e i dispari in modo decrescente,
+    disposti nella sequenza:
+    - pari crescente
+    - dispari decrescente
+    Esempio del contenuto dell'array: 3,4,2,5,6,1,7,8,9 --> 2,4,6,8,9,7,5,3,1
+    */
+    private static int[] evenOddSearch(int[] array) {
+        int[] finalArray = new int[array.length];
+        int[] evenArray = new int[array.length];
+        int[] oddArray = new int[array.length];
+
+
+
+        return finalArray;
     }
+
     private static void biBinarySearch() {
 
     }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SearchMethod
+This project done in java has the purpose of the creation of 3 
+method: <br>
+- wrongIndexSearch;
+- evenOddSearch;
+- biBinarySearch.
+
+## wrongIndexSearch
+
+
+## evenOddSearch
+
+
+## biBinarySearch
