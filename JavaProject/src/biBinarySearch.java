85961376c59d9e1989a733ff2337fcd5759bc246
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2024-04-21 17:38:14 +0200

    Write the docs of biBinarySearch methods

--- JavaProject/src/biBinarySearch.java
+++ JavaProject/src/biBinarySearch.java
@@ -71,6 +71,13 @@ public class biBinarySearch {
         return -1;
     }
 
+    /**
+     * This method sort the array passed as param.<br>
+     * First puts all the even numbers (of the array passed as param) in ascending order.<br>
+     * Last puts all the odd number (of the array passed as param) in descending order.
+     * @param arr the array which we want ot sort.
+     * @return the final array with first the even numbers and last the odd ones.
+     */
     private static int[] evenOddSearch(int[] arr) {
         int[] finalArray = new int[arr.length]; // Array where there is first even and last odd numbers
         int[] evenArray = new int[arr.length]; // Array to store the even numbers
@@ -100,6 +107,16 @@ public class biBinarySearch {
         return finalArray;
     }
 
+    /**
+     * Merge two array in one.<br>
+     * First puts the elements (from pos 0 to indexArr1 - 1) of the arr1 and then
+     * the elements (from pos 0 to indexArr1 - 1) of arr2.
+     * @param arr1 The first array.
+     * @param indexArr1 the position (of arr1) after the last inset number.
+     * @param arr2 the second array.
+     * @param indexArr2 the position (of arr2) after the last inset number.
+     * @return an array with, first the element of arr1, last the element of arr2.
+     */
     private static int[] mergeArrays(int[] arr1, int indexArr1, int[] arr2, int indexArr2) {
         int[] finalArray = new int[indexArr1 + indexArr2];
         int indexFinal = 0;
@@ -115,6 +132,11 @@ public class biBinarySearch {
         return finalArray;
     }
 
+    /**
+     * Order arr param in ascending order.
+     * @param arr the array which wa want to order
+     * @param index the index of where to stop the order
+     */
     private static void orderUpArray(int[] arr, int index) {
         int numMin, posOfnMin, temp;
 
@@ -136,6 +158,11 @@ public class biBinarySearch {
         }
     }
 
+    /**
+     * Order arr param in descending order.
+     * @param arr the array which wa want to order.
+     * @param index the index of where to stop the order.
+     */
     private static void orderDownArray(int[] arr, int index) {
         int numMax, posOfnMax = 0, temp;
 
